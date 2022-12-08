0bc5f77627399cee1bb52a8f12f0c7f298b510f3
JuOliv <julianaoliveira@MacBook-Pro.local>
2022-12-07 22:43:37 -0300

    criação de fases finais

--- scripts/main.js
+++ scripts/main.js
@@ -30,6 +30,7 @@ function createGame(jogador1, hora, jogador2, penaltis){
             />`
     }
     resp = resp + `</li>`
+
     return resp
     /* return `
         <li>
@@ -87,7 +88,6 @@ document.querySelector("#oitavas").innerHTML = `
     )} 
 `
 document.querySelector("#quartas").innerHTML = `
-    
     ${createCard(
         "03/12",
         "sexta",
@@ -102,100 +102,122 @@ document.querySelector("#quartas").innerHTML = `
     )} 
 `
 
-document.querySelector("#cards").innerHTML =
-    createCard("20/11", "domingo", createGame("qatar", "0 X 2", "ecuador")) +
-    createCard(
+document.querySelector("#semi").innerHTML = `
+    ${createCard(
+        "13/12",
+        "terça",
+            createGame("", "16:00", "")
+    )} 
+    ${createCard(
+        "14/12",
+        "quarta",
+            createGame("", "16:00", "")
+    )} 
+`
+
+document.querySelector("#terceiro").innerHTML = ` 
+    ${createCard("17/12", "sábado", createGame("", "12:00", ""))} 
+`
+
+document.querySelector("#final").innerHTML = ` 
+    ${createCard("18/12", "domingo", createGame("", "12:00", ""))} 
+`
+
+document.querySelector("#cards").innerHTML = ` 
+    ${createCard("20/11", "domingo", createGame("qatar", "0 X 2", "ecuador"))} 
+    ${createCard(
         "21/11",
         "segunda",
         createGame("england", "6 X 2", "iran") +
             createGame("senegal", "0 X 2", "netherlands") +
             createGame("united states", "1 X 1", "wales")
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         "22/11",
         "terça",
         createGame("argentina", "1 X 2", "sahrawi arab democratic republic") +
             createGame("denmark", "1 X 0", "tunisia") +
             createGame("mexico", "0 X 0", "poland") +
             createGame("france", "4 X 1", "australia")
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         "23/11",
         "quarta",
         createGame("morocco", "0 X 0", "croatia") +
             createGame("germany", "1 X 2", "japan") +
             createGame("spain", "7 X 0", "costa rica") +
             createGame("belgium", "1 X 0", "canada")
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `24/11`,
         `quinta`,
         createGame(`switzerland`, `1 X 0`, `cameroon`) +
             createGame(`uruguay`, `0 X 0`, `south korea`) +
             createGame(`portugal`, `3 X 2`, `ghana`) +
             createGame(`brazil`, `2 X 0`, `serbia`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `25/11`,
         `sexta`,
         createGame(`wales`, `0 X 2`, `iran`) +
             createGame(`qatar`, `1 X 3`, `senegal`) +
             createGame(`netherlands`, `1 X 1`, `ecuador`) +
             createGame(`england`, `0 X 0`, `united states`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `26/11`,
         `sábado`,
         createGame(`tunisia`, `0 X 1`, `australia`) +
             createGame(`poland`, `2 X 0`, `sahrawi arab democratic republic`) +
             createGame(`france`, `2 X 1`, `denmark`) +
             createGame(`argentina`, `2 X 0`, `mexico`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `27/11`,
         `domingo`,
         createGame(`japan`, `0 X 1`, `costa rica`) +
             createGame(`belgium`, `0 X 2`, `morocco`) +
             createGame(`croatia`, `4 X 1`, `canada`) +
             createGame(`spain`, `1 X 1`, `germany`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `28/11`,
         `segunda`,
         createGame(`cameroon`, `3 X 3`, `serbia`) +
             createGame(`south korea`, `2 X 3`, `ghana`) +
             createGame(`brazil`, `1 X 0`, `switzerland`) +
             createGame(`portugal`, `2 x 0`, `uruguay`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `29/11`,
         `terça`,
         createGame(`ecuador`, `1 X 2`, `senegal`) +
             createGame(`netherlands`, `2 X 0`, `qatar`) +
             createGame(`wales`, `0 X 3`, `england`) +
             createGame(`iran`, `0 X 1`, `united states`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         "30/11",
         "quarta",
         createGame("australia", `1 X 0`, "denmark") +
             createGame("tunisia", `1 X 0`, "france") +
             createGame("poland", `0 X 2`, "argentina") +
             createGame("sahrawi arab democratic republic", `1 X 2`, "mexico")
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `01/12`,
         `quinta`,
         createGame(`croatia`, `0 X 0`, `belgium`) +
             createGame(`canada`, `1 X 2`, `morocco`) +
             createGame(`japan`, `2 X 1`, `spain`) +
             createGame(`costa rica`, `2 X 4`, `germany`)
-    ) +
-    createCard(
+    )} 
+    ${createCard(
         `02/12`,
         `sexta`,
         createGame(`ghana`, `0 X 2`, `uruguay`) +
             createGame(`south korea`, `2 X 1`, `portugal`) +
             createGame(`serbia`, `2 X 3`, `switzerland`) +
             createGame(`cameroon`, `1 X 0`, `brazil`)
-    ) 
+    )} 
+`
